15ba40138b2b0a6799820e63183a6178ae35d77d
TenGreen <Grishintut@yandex.ru>
2023-03-30 16:57:15 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+
     }
 }
